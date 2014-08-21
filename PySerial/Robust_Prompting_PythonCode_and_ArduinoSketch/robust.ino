824a9a15ab2973c61c5050f09bc5d89f67922e5e
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-08-21 11:50:19 -0700

    Switched if-else for switch-case

--- PySerial/Robust_Prompting_PythonCode_and_ArduinoSketch/robust.ino
+++ PySerial/Robust_Prompting_PythonCode_and_ArduinoSketch/robust.ino
@@ -1,4 +1,4 @@
-char ch;
+char char_input;
 
 void setup () {
   pinMode(13, OUTPUT);
@@ -8,23 +8,25 @@ void setup () {
 
 void loop() {
 
-  if (Serial.available()) 
+  if (Serial.available() > 0) 
   {
-
-     ch = Serial.read();
-
-     if ( ch == '1' ) { 
-       Serial.println(analogRead(A0)); // if '1' is received, then send back analog read A0
-     } 
-     else if (ch == '2') {    
+      
+      char_input = Serial.read();
+      
+      switch (char_input) {
+      case '1':
+        Serial.println(analogRead(A0)); // if '1' is received, then send back analog read A0
+      break;
+      case '2':
        digitalWrite(13,HIGH); // if '2' is received, turn on the led attached to 13
-     } 
-     else if (ch == '3') {
+      break;
+      case '3':
        digitalWrite(13,LOW); // if '3' is received then turn off the led attached 13
-     } else {
-       delay(10);
-     }
-
+      break;
+      default: 
+        delay(10);
+      break;
+      }
   }
 
 }
